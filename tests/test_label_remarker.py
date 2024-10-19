353fd1ad26d59a1d6260b68ca26bfd0272e54a80
ivanxiedata <ivanxiedata@gmail.com>
2024-10-19 15:45:22 -0500

    pytest bandit

--- tests/test_label_remarker.py
+++ tests/test_label_remarker.py
@@ -3,11 +3,10 @@ from AutoLabel.main import Label_Remarker
 
 def test_contains_chinese():
     lr = Label_Remarker()
-    assert lr.contains_chinese('你好') == True
-    assert lr.contains_chinese('Hello') == False
+    assert lr.contains_chinese('你好')
+    assert not lr.contains_chinese('Hello')
 
 def test_validate_product_name():
     lr = Label_Remarker()
     assert lr.validate_product_name('Wrist') == 'Wrist ball'
     assert lr.validate_product_name('Unknown') == 'Unknown'
-
