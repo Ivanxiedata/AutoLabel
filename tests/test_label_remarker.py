7394e5dd763e9276b8150bef1a871e718336f375
ivanxiedata <ivanxiedata@gmail.com>
2024-10-19 16:02:51 -0500

    add more tests

--- tests/test_label_remarker.py
+++ tests/test_label_remarker.py
@@ -1,5 +1,6 @@
 import pytest
 from pytest_assume.plugin import assume
+import pandas as pd
 from AutoLabel.main import Label_Remarker
 
 def test_contains_chinese():
@@ -13,3 +14,41 @@ def test_validate_product_name():
     with assume:  # Multiple checks in one test function
         assume(lr.validate_product_name('Wrist') == 'Wrist ball')
         assume(lr.validate_product_name('Unknown') == 'Unknown')
+
+def test_find_col_name():
+    lr = Label_Remarker()
+    mock_data = {
+        'The title of the product': ['Product A', 'Product B'],
+        'The total quantity': [10, 20]
+    }
+    df = pd.DataFrame(mock_data)
+
+    with assume:
+        assume(lr.find_column_name(df,'The title of the product') == 'The title of the product')
+        assume(lr.find_column_name(df, 'The total') == 'The total quantity')
+
+        try:
+            lr.find_column_name(df, 'Non-Existent Column')
+            assume(False)
+        except KeyError:
+            assume(True)
+
+def test_g_translator(mocker):
+    lr = Label_Remarker()
+
+    # Mock the Translator's translate method to simulate translation
+    # Replace the actual translation in the original function with mock data
+    mock_translate = mocker.patch('AutoLabel.main.Translator.translate')
+    # simulate a successful translation response
+    mock_translate.return_value.text = 'Hello'
+
+    with assume:
+        assume(lr.g_translator('你好') == 'Hello')
+        mock_translate.assert_called_once_with('你好', src='zh-cn', dest='en')
+
+        assume(lr.g_translator('Car' == 'car'))
+        mock_translate.reset_mock()
+
+        mock_translate.side_effect = Exception('Translation failed')
+        assume(lr.g_translator('你好') == '你好')
+
